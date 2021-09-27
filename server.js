217bbd6fa70003b7eedce6effe7ebdb1f6a596b8
Lee Grisham <leegrisham@gmail.com>
2021-09-27 14:41:30 -0500

    add next()

--- server.js
+++ server.js
@@ -27,9 +27,10 @@ const app = express();
     }));
 
     app.use((req,res, next)=>{
-              res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
         res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+        next();
     }
     )
 
